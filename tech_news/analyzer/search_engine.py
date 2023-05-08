40b17e666251b1ea7fb7bb3fdde2217e79f3c54b
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-08 18:14:08 -0400

    feat: add search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -13,7 +14,14 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        dateFormated = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        return [
+            (article["title"], article["url"])
+            for article in search_news({"timestamp": {"$eq": dateFormated}})
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
