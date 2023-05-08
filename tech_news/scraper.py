1080b4626c960b3f4b09c3e0dc9106b65f942d03
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-08 17:19:03 -0400

    feat: add scrape_next_page_link function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,12 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    try:
+        selector = Selector(text=html_content)
+        next_page_link = selector.css(".next.page-numbers::attr(href)").get()
+        return next_page_link
+    except Exception:
+        return None
 
 
 # Requisito 4
