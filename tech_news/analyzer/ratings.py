7e9ff014605d9eace76b7ee7a8200fd8624e53e5
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-08 18:23:39 -0400

    feat: add top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,12 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = [new["category"] for new in find_news()]
+    categories = [
+        category[0]
+        for category in Counter(sorted(categories)).most_common(5)
+    ]
+    return categories
